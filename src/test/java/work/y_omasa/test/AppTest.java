9b46b91ebc0a11e312df94195f685495e5f31b2a
takenakayuki <kbc18a06@stu.kawahara.ac.jp>
2020-07-31 11:45:21 +0900

    修正

--- src/test/java/work/y_omasa/test/AppTest.java
+++ src/test/java/work/y_omasa/test/AppTest.java
@@ -1,15 +1,10 @@
 package work.y_omasa.test;
-
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
-
 import org.junit.Test;
-
 public class AppTest {
-
     // メールアドレスを正しい書式で設定し、同じ値を取得する
     @Test
     public void setEmailWithCollectFormatAndGetMatchingEmail() {
@@ -21,58 +16,50 @@ public class AppTest {
         }
         assertThat(user.getEmail(), is("test@example.com"));
     }
-
     // メールアドレスを誤った書式で設定した場合、例外が発生する
-    @Test(expected = NumberFormatException.class)
-    public void setEmailWithWrongFormatResultException() throws Exception{
+    @Test(expected = InputCheckException.class)
+    public void setEmailWithWrongFormatResultException() throws Exception {
         User user = new User();
         user.setEmail("testexample.com");
     }
-
-    //パスワードのハッシュを設定し、正しいパスワードをチェックするとtrue
+    // パスワードのハッシュを設定し、正しいパスワードをチェックするとtrue
     @Test
-    public void setPasswordHashAndCheckCollectPasswordReturnTrue()
-    {
+    public void setPasswordHashAndCheckCollectPasswordReturnTrue() {
         String pwd = "1234";
         User user = new User();
         MessageDigest digest = null;
         try {
-            //渡された文字列からハッシュを生成 
-			digest = MessageDigest.getInstance("MD5");
-			digest.reset();
-			digest.update(pwd.getBytes("utf8"));
+            // 渡された文字列からハッシュを生成
+            digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(pwd.getBytes("utf8"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //生成されたハッシュ値を文字列に変換
+        // 生成されたハッシュ値を文字列に変換
         String hashedPw = String.format("%032x", new BigInteger(1, digest.digest()));
         // userにパスワードハッシュを設定
-        user.setPwdHash(String.valueOf(hashedPw));        
-
-        assertTrue( user.isPwdValid("abcd") );
+        user.setPwdHash(String.valueOf(hashedPw));
+        assertTrue(user.isPwdValid("1234"));
     }
-
-    //パスワードのハッシュを設定し、誤ったパスワードをチェックするとfalse
+    // パスワードのハッシュを設定し、誤ったパスワードをチェックするとfalse
     @Test
-    public void setPasswordHashAndCheckWrongPasswordReturnFalse()
-    {
+    public void setPasswordHashAndCheckWrongPasswordReturnFalse() {
         User user = new User();
         String pwd = "1234";
         MessageDigest digest = null;
         try {
-            //渡された文字列からハッシュを生成 
-			digest = MessageDigest.getInstance("MD5");
-			digest.reset();
-			digest.update(pwd.getBytes("utf8"));
+            // 渡された文字列からハッシュを生成
+            digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(pwd.getBytes("utf8"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //生成されたハッシュ値を文字列に変換
+        // 生成されたハッシュ値を文字列に変換
         String hashedPw = String.format("%032x", new BigInteger(1, digest.digest()));
         // userにパスワードハッシュを設定
-        user.setPwdHash(String.valueOf(hashedPw));        
-
-        assertFalse( user.isPwdValid("2345") );
+        user.setPwdHash(String.valueOf(hashedPw));
+        assertFalse(user.isPwdValid("2345"));
     }
-
 }
